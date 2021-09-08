9948675e3c0eb84b07c45840bd8000924ad3657f
Ethan Daya <me@Ethandaya.com>
2021-09-08 19:28:01 +1000

    fix: add approval

--- pages/migrate.tsx
+++ pages/migrate.tsx
@@ -5,11 +5,14 @@ import {Header} from "components/Header";
 import Head from "next/head";
 import {ContractContext} from "providerts/ContractProvider";
 import {buildMigrationPayload} from "utils/migrate";
+import {SETTLEMENT_V2_CONTRACT_ADDRESS} from "constants/addresses";
+import useSWR from "swr";
+import {useWeb3React} from "@web3-react/core";
 
 function Migrate() {
 
   const [tokenIds, setTokenIds] = useState(['']);
-
+  const {account} = useWeb3React()
   const {STL, STLV2, isReadOnly} = useContext(ContractContext)
   const {handleTx, txStatus: harvestStatus, txInProgress: harvIP, txError} = useContractTransaction(1)
 
@@ -21,6 +24,14 @@ function Migrate() {
     newTokenIds[index] = event.target.value;
     setTokenIds(newTokenIds);
   }, [tokenIds]);
+
+
+  const {
+    data: approved,
+    mutate: revalidateApproval
+  } = useSWR(['isApprovedForAll', account],
+    (_, account) => STL.isApprovedForAll(account, SETTLEMENT_V2_CONTRACT_ADDRESS)
+  )
 
   const onMigrate = useCallback(async () => {
     try {
@@ -39,6 +50,43 @@ function Migrate() {
       console.error(err)
     }
   }, [STL, STLV2, handleTx, isReadOnly, tokenIds])
+
+  const approveV2 = useCallback(async () => {
+    try {
+      if (!STL || !STLV2 || isReadOnly) {
+        throw new Error('Contract is not authorised')
+      }
+      const tx = STL.setApprovalForAll(SETTLEMENT_V2_CONTRACT_ADDRESS, true);
+      await handleTx(tx);
+      await revalidateApproval()
+    } catch (e) {
+      console.error(e)
+    }
+  }, [STL, STLV2, handleTx, isReadOnly, revalidateApproval])
+
+  if (!approved) {
+    return (<Fragment>
+        <Head>
+          <title>Migrate</title>
+          <meta name="description" content="Migrate your Settlements"/>
+        </Head>
+        <Header/>
+        <main className={styles.main}>
+          <h1>Migrate</h1>
+          <p>Before you can migrate {"you'll"} need to approve the new V2 Contract</p>
+          <button onClick={approveV2} disabled={harvIP} role="button">
+            {harvestStatus === WalletCallStatus.ERRORED ? (
+              `Try Again`
+            ) : harvestStatus === WalletCallStatus.PROMPTED ? (
+              'Confirm the transaction in your wallet'
+            ) : harvestStatus === WalletCallStatus.CONFIRMING ? (
+              <span>confirming</span>
+            ) : 'Approve your Settlements'}
+          </button>
+        </main>
+      </Fragment>
+    )
+  }
 
   return (
     <Fragment>
