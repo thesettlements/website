0c1fb47daf680802a495ef1688590c2ec25e361c
Ethan Daya <me@Ethandaya.com>
2021-09-08 19:24:29 +1000

    chore: show error

--- pages/migrate.tsx
+++ pages/migrate.tsx
@@ -1,7 +1,6 @@
 import {useContractTransaction, WalletCallStatus} from "hooks/useContractTx";
-import {useRouter} from "next/router";
 import styles from 'styles/Migrate.module.scss';
-import React, {Fragment, useState, useContext, useCallback} from "react";
+import React, {Fragment, useCallback, useContext, useState} from "react";
 import {Header} from "components/Header";
 import Head from "next/head";
 import {ContractContext} from "providerts/ContractProvider";
@@ -10,9 +9,9 @@ import {buildMigrationPayload} from "utils/migrate";
 function Migrate() {
 
   const [tokenIds, setTokenIds] = useState(['']);
-  
+
   const {STL, STLV2, isReadOnly} = useContext(ContractContext)
-  const {handleTx, txStatus: harvestStatus, txInProgress: harvIP } = useContractTransaction(1)
+  const {handleTx, txStatus: harvestStatus, txInProgress: harvIP, txError} = useContractTransaction(1)
 
   const onAddAnother = useCallback(() => setTokenIds([...tokenIds, '']), [tokenIds]);
 
@@ -29,8 +28,8 @@ function Migrate() {
         throw new Error('Contract is not authorised')
       }
       const filteredTokenIds = tokenIds.filter((id) => !!id);
-      const attributePromises =  filteredTokenIds.map((id) => buildMigrationPayload(id, STL));
-      if (attributePromises.length == 0) {
+      const attributePromises = filteredTokenIds.map((id) => buildMigrationPayload(id, STL));
+      if (attributePromises.length === 0) {
         return;
       }
       const attributes = await Promise.all(attributePromises);
@@ -43,34 +42,37 @@ function Migrate() {
 
   return (
     <Fragment>
-    <Head>
-      <title>Migrate</title>
-      <meta name="description" content="Migrate your Settlements"/>
-    </Head>
-    <Header/>
-    <main className={styles.main}>
-      <h1>Migrate</h1>
-      <div className={styles.migrate}>
-        {tokenIds.map((_, index) => 
-            <input className={styles.input} id={index.toString()} key={index} placeholder={'Token ID'} onChange={onChange}/>
-        )}
-        <div>
-          <button className={styles.button} style={{marginRight: 20}} role="button" onClick={onAddAnother}>
-            Add another
-          </button>
-          <button className={styles.button} role="button" onClick={onMigrate}>
-          {harvestStatus === WalletCallStatus.ERRORED ? (
-            `Try Again`
-          ) : harvestStatus === WalletCallStatus.PROMPTED ? (
-            'Confirm the transaction in your wallet'
-          ) : harvestStatus === WalletCallStatus.CONFIRMING ? (
-            'confirming...'
-          ) : (tokenIds.length > 1 ? `Migrate ${tokenIds.length} Settlements` : 'Migrate')}
-          </button>
+      <Head>
+        <title>Migrate</title>
+        <meta name="description" content="Migrate your Settlements"/>
+      </Head>
+      <Header/>
+      <main className={styles.main}>
+        <h1>Migrate</h1>
+        <div className={styles.migrate}>
+          {tokenIds.map((_, index) =>
+            <input className={styles.input} id={index.toString()} key={index} placeholder={'Token ID'}
+                   onChange={onChange}/>
+          )}
+          <div>
+            <button className={styles.button} style={{marginRight: 20}} role="button" onClick={onAddAnother}>
+              Add another
+            </button>
+            <button className={styles.button} role="button" onClick={onMigrate}>
+              {harvestStatus === WalletCallStatus.ERRORED ? (
+                `Try Again`
+              ) : harvestStatus === WalletCallStatus.PROMPTED ? (
+                'Confirm the transaction in your wallet'
+              ) : harvestStatus === WalletCallStatus.CONFIRMING ? (
+                'confirming...'
+              ) : (tokenIds.length > 1 ? `Migrate ${tokenIds.length} Settlements` : 'Migrate')}
+            </button>
+          </div>
+          {txError && <p style={{color: 'red'}}> {txError?.message}</p>}
         </div>
-      </div>
-    </main>
-  </Fragment>)
+      </main>
+    </Fragment>
+  )
 }
-  
+
 export default Migrate;
